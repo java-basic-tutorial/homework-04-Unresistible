8d05dd0f1e2fe30a112a9c93fe0dc760a09fa247
Vova.Matsak <vmats@softserveinc.com>
2022-01-19 15:01:02 +0200

    Homework 4. task 3/3 pass

--- src/main/java/com/softserveinc/task03/UserManager.java
+++ src/main/java/com/softserveinc/task03/UserManager.java
@@ -1,8 +1,7 @@
 package com.softserveinc.task03;
 
-//copy-pasted 3
+//copy-pasted 2/3
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 public class UserManager {
@@ -12,4 +11,41 @@ public class UserManager {
     public UserManager() {
         users = new HashSet<>();
     }
+
+    public void register(String login, String password){
+        users.add(new User(login, password));
+    }
+
+    public void delete(String login){
+        for (User user: users) {
+            if (user.getLogin().equals(login)) {
+                this.users.remove(user);
+                break;
+            }
+        }
+    }
+
+    public boolean exists(String login, String password) {
+        for (User user: users) {
+            if (user.getLogin().equals(login)) {
+                if (user.getPassword().equals(password)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean changePassword(String login, String oldPassword, String newPassword) {
+        for (User user: users) {
+            if (user.getLogin().equals(login)) {
+                if (user.getPassword().equals(oldPassword)) {
+                    user.setPassword(newPassword);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
 }
